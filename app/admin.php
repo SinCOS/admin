2e5478ff7c567d35250ac1842841962626fa6191
qyf1994 <qyf1994@gmail.com>
2017-04-17 21:48:44 +0800

    sdfasdf

--- app/admin.php
+++ app/admin.php
@@ -1,54 +1,71 @@
 <?php
 
-    $app->group('/admin',function()use($app){
-        $app->get('/',function($rst,$resp,$args){
+    $app->group('/admin', function () use ($app) {
+        $app->get('/', function ($rst, $resp, $args) {
             return $resp;
         });
-        $app->get('/login',"AuthController:getSignUp")->setName('auth.signup');
-        $app->post('/login','AuthController:postSignUp');
-        $app->get('/group/id',function($rst,$resp,$args){
+        $app->get('/login', "AuthController:getSignUp")->setName('auth.signup');
+        $app->post('/login', 'AuthController:postSignUp');
+        $app->get('/group/id', function ($rst, $resp, $args) {
 
-              $list = $this->db->select("user_stock",'*',[
+              $list = $this->db->select("user_stock", '*', [
                   'AND' =>[
                   'status[>]' => 0,
-                  'uid' => 1 
-                ]
-            ]);
+                  'uid' => 1
+                  ]
+              ]);
        
-        $resp->getBody()->write(var_export($list));
-        return $resp;
-    });
-     $app->get('/member/list',"MemberController:getMembers");
-     $app->get('/stock/public',function($rst,$resp,$args){
-        if(!$rst->isXhr()){
-            $list = $this->db->select('stockGroup','*',[
-                    'uid' => 0,
-                    'ORDER' => ['id' => 'ASC']
-            ]);
-            return $this->view->render($resp,'template/stock/public.html',['group_list'=> $list ??[]]);
-        }
-
-     });
-    $app->get('/stock/group/{group_id:[0-9]+}',"StockController:getGroupStock");
-    $app->get('/group/{uid:[0-9]+}/uid',function($rst,$resp,$args){
-        $list = $this->db->select('stockGroup','*',[
+            $resp->getBody()->write(var_export($list));
+            return $resp;
+        });
+        $app->get('/member/list', "MemberController:getMembers");
+        $app->get('/stock/public', function ($rst, $resp, $args) {
+            if (!$rst->isXhr()) {
+                $list = $this->db->select('stockGroup', '*', [
+                  'uid' => 0,
+                  'ORDER' => ['id' => 'ASC']
+                ]);
+                 return $this->view->render($resp, 'template/stock/public.html', ['group_list'=> $list ??[]]);
+            }
+        });
+        $app->get('/stock/info', function ($rst, $resp) {
+              $data = file_get_contents('http://120.24.184.121/public/cpy_info.json');
+              echo $data;
+        });
+        $app->delete('/stock/{group_id:[0-9]+}/{id:[0-9]+}', function ($rst, $resp, $args) {
+             $res_id = $this->db->delete('stockGroup', [
+            'AND' => [
+                'uid' => 0,
+                'sg_id' => intval($args['group_id']),
+                'id' => intval($args['id'])
+            ]
+              ]);
+              return $resp->getBody()->write(json_encode([
+            'status' => $res_id ? 200 :400,
+            'message' => '',
+            'result' => []
+              ]));
+        });
+        $app->get('/stock/group/{group_id:[0-9]+}', "StockController:getGroupStock");
+        $app->get('/group/{uid:[0-9]+}/uid', function ($rst, $resp, $args) {
+            $list = $this->db->select('stockGroup', '*', [
             "AND" => [
                 'status[>]' => 0,
                 'uid' => intval($args['uid'])
             ]
-        ]);
-        $resp->getBody()->write(json_encode([
+            ]);
+            $resp->getBody()->write(json_encode([
              'status' => $err_code ?? 200,
              'msg' => $msg ?? 'ok',
              'result' => $list
-         ]));
-        return $resp;
-    });
-    $app->get('/group/{gid:[0-9]+}/detail',function($rst,$resp,$args){
-            $list = $this->db->select('user_stock','*',[
+             ]));
+            return $resp;
+        });
+        $app->get('/group/{gid:[0-9]+}/detail', function ($rst, $resp, $args) {
+            $list = $this->db->select('user_stock', '*', [
                 'AND' => [
                     'sg_id' => intval($args['gid']),
-                    'status[>]' => 0  
+                    'status[>]' => 0
                 ]
             ]);
             return $resp->getBody()->write(json_encode(
@@ -58,21 +75,19 @@
                     'result' => $list ?? []
                 ]
             ));
-    });
+        });
 
-    $app->get('/tickets',function($rst,$resp,$args){
-        $this->logger->addInfo('Ticket list');
-        $resp->getBody()->write(var_export($rst->getQueryParams()));
-        return $resp;
+        $app->get('/tickets', function ($rst, $resp, $args) {
+            $this->logger->addInfo('Ticket list');
+            $resp->getBody()->write(var_export($rst->getQueryParams()));
+            return $resp;
+        });
+        $app->get('/user/login', function ($rst, $resp, $args) {
+            $data = $rst->getQueryParams();
+            $user['email'] = filter_var($data['email'], FILTER_VALIDATE_EMAIL);
+        });
+        $app->post('/tickets', function ($rst, $resp, $args) {
+            $data = $rst->getParsedBody();
+            $ticket_data = [];
+        });
     });
-    $app->get('/user/login',function($rst,$resp,$args){
-        $data = $rst->getQueryParams();
-        $user['email'] = filter_var($data['email'],FILTER_VALIDATE_EMAIL);
-    });
-    $app->post('/tickets',function($rst,$resp,$args){
-        $data = $rst->getParsedBody();
-        $ticket_data = [];
-
-    });
-});
-
